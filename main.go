1787ab6f51a9a10143cbaff99af9ea5841f8e087
Rafael Vicio <rafaelvicio@icloud.com>
2018-06-17 18:09:31 -0300

    part 1

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+)
+
+//Roll is a model
+type Roll struct {
+	ID          string `json:"id"`
+	ImageNumber string `json:"imagemNumber"`
+	Name        string `json:"name"`
+	Ingredients string `json:"ingredients"`
+}
+
+var rolls []Roll
+
+func getRolls(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(rolls)
+}
+
+func getRoll(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	for _, item := range rolls {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+}
+
+func createRoll(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var newRoll Roll
+	json.NewDecoder(r.Body).Decode(&newRoll)
+	newRoll.ID = strconv.Itoa(len(rolls) + 1)
+	rolls = append(rolls, newRoll)
+	json.NewEncoder(w).Encode(newRoll)
+}
+
+func updateRoll(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func deleteRoll(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func main() {
+
+	rolls = append(rolls, Roll{ID: "1", ImageNumber: "8", Name: "Spicy Tuna Roll", Ingredients: "Tuna, Chili sauce, Nori, Rice"})
+
+	router := mux.NewRouter()
+
+	//endpoints
+	router.HandleFunc("/sushi", getRolls).Methods("GET")
+	router.HandleFunc("/sushi/{id}", getRoll).Methods("GET")
+	router.HandleFunc("/sushi", createRoll).Methods("POST")
+	router.HandleFunc("/sushi/{id}", updateRoll).Methods("PUT")
+	router.HandleFunc("/sushi/{id}", deleteRoll).Methods("DELETE")
+
+	log.Fatal(http.ListenAndServe(":5000", router))
+
+}
